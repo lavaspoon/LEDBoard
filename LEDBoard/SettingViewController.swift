74199633b1d1a3304fb116d4c01230a314fad9b2
lavaspoon <lavaspoon@naver.com>
2022-08-19 23:24:03 +0900

    세팅뷰 버튼 connected

---
+++ LEDBoard/SettingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by spoon lava on 2022/08/19.
+//
+
+import UIKit
+
+class SettingViewController : UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var yellowBtn: UIButton!
+    @IBOutlet weak var purpleBtn: UIButton!
+    @IBOutlet weak var greenBtn: UIButton!
+    @IBOutlet weak var blackBtn: UIButton!
+    @IBOutlet weak var blueBtn: UIButton!
+    @IBOutlet weak var orangeBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func tapTextColorBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapBackGroundColorBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapSaveBtn(_ sender: UIButton) {
+        
+    }
+}
