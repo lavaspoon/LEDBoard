1129ef3776344e40c63f6c9b4d5a14d054e6e896
lavaspoon <lavaspoon@naver.com>
2022-08-22 23:36:23 +0900

    세팅뷰 컨트롤러 데이터 델리게이트를 통한 뷰 컨트롤러로 데이터 전달

--- LEDBoard/SettingViewController.swift
+++ LEDBoard/SettingViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+// 1. 델리게이트 프로토콜 정의
+protocol LEDBoardSettingDelegate : AnyObject {
+    func changeSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
+}
+
 class SettingViewController : UIViewController {
     
     @IBOutlet weak var textField: UITextField!
@@ -17,29 +22,41 @@ class SettingViewController : UIViewController {
     @IBOutlet weak var blueBtn: UIButton!
     @IBOutlet weak var orangeBtn: UIButton!
     
+// 2. 델리게이트 프로퍼티 추가
+    weak var delegate: LEDBoardSettingDelegate?
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func tapTextColorBtn(_ sender: UIButton) {
         if sender == self.yellowBtn {
             self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
         } else if sender == self.purpleBtn {
             self.changeTextColor(color: .purple)
+            self.textColor = .purple
         } else {
             self.changeTextColor(color: .green)
+            self.textColor = .green
         }
     }
     @IBAction func tapBackGroundColorBtn(_ sender: UIButton) {
         if sender == self.blackBtn {
             self.changeBackgroundColorButton(color: .black)
+            self.backgroundColor = .black
         } else if sender == self.blueBtn {
             self.changeBackgroundColorButton(color: .blue)
+            self.backgroundColor = .blue
         } else {
             self.changeBackgroundColorButton(color: .orange)
+            self.backgroundColor = .orange
         }
     }
     @IBAction func tapSaveBtn(_ sender: UIButton) {
-        
+        self.delegate?.changeSetting(text: self.textField.text, textColor: self.textColor, backgroundColor: self.backgroundColor)
+        self.navigationController?.popViewController(animated: true)
     }
     private func changeTextColor(color: UIColor) {
         //파라미터로 전달받은 컬러가 노란색이면 1로 세팅
--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -7,14 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+    
+    func changeSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
+    
 
     @IBOutlet weak var contentsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.contentsLabel.textColor = .yellow
     }
-
+    
+    //세그웨이로 화면전환 했기 떄문에 prepare 메서드 오버라이드
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController { //다운캐스팅
+            settingViewController.delegate = self
+        }
+    }
 
 }
 
