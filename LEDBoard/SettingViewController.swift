a291af2a97334f3396c7697975b153b2f8eed5ac
lavaspoon <lavaspoon@naver.com>
2022-08-22 23:26:08 +0900

    텍스트 색상, 배경 색상 변경 함수 기능 추가

--- LEDBoard/SettingViewController.swift
+++ LEDBoard/SettingViewController.swift
@@ -21,12 +21,35 @@ class SettingViewController : UIViewController {
         super.viewDidLoad()
     }
     @IBAction func tapTextColorBtn(_ sender: UIButton) {
-        
+        if sender == self.yellowBtn {
+            self.changeTextColor(color: .yellow)
+        } else if sender == self.purpleBtn {
+            self.changeTextColor(color: .purple)
+        } else {
+            self.changeTextColor(color: .green)
+        }
     }
     @IBAction func tapBackGroundColorBtn(_ sender: UIButton) {
-        
+        if sender == self.blackBtn {
+            self.changeBackgroundColorButton(color: .black)
+        } else if sender == self.blueBtn {
+            self.changeBackgroundColorButton(color: .blue)
+        } else {
+            self.changeBackgroundColorButton(color: .orange)
+        }
     }
     @IBAction func tapSaveBtn(_ sender: UIButton) {
         
+    }
+    private func changeTextColor(color: UIColor) {
+        //파라미터로 전달받은 컬러가 노란색이면 1로 세팅
+        self.yellowBtn.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.purpleBtn.alpha = color == UIColor.purple ? 1 : 0.2
+        self.greenBtn.alpha = color == UIColor.green ? 1 : 0.2
+    }
+    private func changeBackgroundColorButton(color: UIColor) {
+        self.blackBtn.alpha = color == UIColor.black ? 1 : 0.2
+        self.blueBtn.alpha = color == UIColor.blue ? 1 : 0.2
+        self.orangeBtn.alpha = color == UIColor.orange ? 1 : 0.2
     }
 }
